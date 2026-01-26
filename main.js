6c76629e6942ffdd0ad2d5e85d1e65f6de6502ae
Claire Woods <claire@flechette.net>
2026-01-25 23:16:29 -0600

    HDC at correct size

--- main.js
+++ main.js
@@ -4,13 +4,14 @@ const { findSteamApp } = require('steam-locate');
 
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
-let mainWindow = null;
+let finderWindow = null;
+let gameWindow = null;
 
 // Initialize `@electron/remote` module
 require('@electron/remote/main').initialize();
 
-const createWindow = () => {
-    mainWindow = new BrowserWindow({
+const createGameFinderWindow = () => {
+    finderWindow = new BrowserWindow({
         width: 800,
         height: 600,
         title: "Finding Hyperspace Deck Command",
@@ -19,57 +20,70 @@ const createWindow = () => {
         }
     });
 
-    mainWindow.loadFile('file_finder.html');
+    finderWindow.loadFile('file_finder.html');
+
+     // Emitted when the window is closed.
+    finderWindow.on("closed", function() {
+        // Dereference the window object, usually you would store windows
+        // in an array if your app supports multi windows, this is the time
+        // when you should delete the corresponding element.
+        finderWindow = null;
+    });
 }
 
 app.whenReady().then(() => {
-    ipcMain.handle('beginGame', beginGame)
+    ipcMain.handle('beginGame', createGameWindow)
     ipcMain.handle('findSteamApp', findSteamGamePath)
-    createWindow()
+    createGameFinderWindow()
 })
 
-// Emitted when the window is closed.
-mainWindow.on("closed", function() {
-    // Dereference the window object, usually you would store windows
-    // in an array if your app supports multi windows, this is the time
-    // when you should delete the corresponding element.
-    mainWindow = null;
-});
+function createGameWindow(_event, gameFilePath){
+    finderWindow.close()
 
-// Quit when all windows are closed.
-app.on("window-all-closed", function() {
-  app.quit();
-});
-
-function beginGame(_event, gameFilePath){
-    mainWindow.width = 1280
-    mainWindow.height = 720
-    mainWindow.useContentSize = true
-    mainWindow.title = "Hyperspace Deck Command: Wishgranter"
-    mainWindow.backgroundColor = '#000000'
-    mainWindow.webPreferences = {
-      // Allow Node.js API access in renderer process, as long
-      // as we've not removed dependency on it and on "@electron/remote".
-      nodeIntegration: true,
-      contextIsolation: false,
-    }
+    // Create the browser window.
+    gameWindow = new BrowserWindow({
+        width: 1280,
+        height: 720,
+        useContentSize: true,
+        title: "Hyperspace Deck Command: Wishgranter",
+        backgroundColor: '#000000',
+        webPreferences: {
+            // Allow Node.js API access in renderer process, as long
+            // as we've not removed dependency on it and on "@electron/remote".
+            nodeIntegration: true,
+            contextIsolation: false,
+        }
+    });
 
     // Enable `@electron/remote` module for renderer process
-    require('@electron/remote/main').enable(mainWindow.webContents);
+    require('@electron/remote/main').enable(gameWindow.webContents);
 
     // Open external link in the OS default browser
-    mainWindow.webContents.setWindowOpenHandler(details => {
+    gameWindow.webContents.setWindowOpenHandler(details => {
         shell.openExternal(details.url);
         return { action: 'deny' };
     });
 
     // and load the index.html of the app.
-    mainWindow.loadFile(gameFilePath + "/resources/app.asar/app/index.html");
+    gameWindow.loadFile(gameFilePath + "/resources/app.asar/app/index.html");
 
     Menu.setApplicationMenu(null);
 
     // Open the DevTools.
-    // mainWindow.webContents.openDevTools()
+    // gameWindow.webContents.openDevTools()
+
+    // Emitted when the window is closed.
+    gameWindow.on("closed", function() {
+        // Dereference the window object, usually you would store windows
+        // in an array if your app supports multi windows, this is the time
+        // when you should delete the corresponding element.
+        gameWindow = null;
+    });
+
+    // Quit when all windows are closed.
+    app.on("window-all-closed", function() {
+        app.quit();
+    });
 }
 
 async function findSteamGamePath(_event, gameID){
