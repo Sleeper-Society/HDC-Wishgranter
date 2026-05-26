677050f3cd65dcab3f5dcd27a66c126cc4d7e6df
Flechette <claire@Flechette.net>
2026-05-25 18:03:35 -0600

    Began changing docs

--- src/modFactory.ts
+++ src/modFactory.ts
@@ -1,3 +1,20 @@
-import type { Mod } from "./mod.ts";
+import type { LoadSequenceFunction } from "wishgranter";
+import type { RuntimeGame } from "./gdjs.ts";
+
+export interface Mod {
+  setDisabled(arg0: boolean): unknown;
+  metadata: ModMetaData;
+  onLoad?: () => ReturnType<LoadSequenceFunction>;
+  onGameStart?: (gdgame: RuntimeGame) => void;
+}
+export interface ModMetaData {
+  name: string;
+  version: string;
+  descr?: string;
+  description?: string;
+  icon?: string;
+  dependencies?: [string, string][];
+  seealso?: [string, string][];
+}
 
 export function pathToMod(path: string): Mod {}
